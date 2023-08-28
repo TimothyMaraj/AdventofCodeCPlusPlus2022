8cbf27e96892bd2493f3c226a76c8259dcfc22b4
ttm51 <ttm51@txstate.edu>
2023-08-27 21:42:18 -0500

    advent of code 2022  pseudo day one

---
+++ DayOne/dayOne.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+
+int main()
+{
+    // Create File variable 
+    // use file var to open file and use it 
+    // for continued i/o
+    // open file and run a loop inside a loop 
+    // the loops loop for all lines in a file and loop over a set of lines 
+    // that contain strings of numbers until there is a full \n new line 
+    // to count over that amount
+
+    // each string should be converted to a number
+    // each number added to a total sum
+    // the sum is compared to the highest_sum 
+    // if sum > h_sum then the sum is the new highest sum
+    // else contine the loop until the h_sum is found
+
+    // print the h_sum 
+}
